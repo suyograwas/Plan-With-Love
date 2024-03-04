6f1cb1fcfa87dbcb20c2738f79a9c99eeab095ec
suyograwas <suyograwas4@gmail.com>
2024-03-04 20:55:39 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Tours from "./Components/Tours";
+import data from "./data";
+
+const App = () => {
+  const [tours, setTours] = useState(data);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  if (tours.length == 0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="app">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Tours.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <div className="container">
+      <div className="">
+        <h2 className="title">Plan With Love ... </h2>
+      </div>
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Tours;
